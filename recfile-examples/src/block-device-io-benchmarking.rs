95bee6f7c099470b6af25bb25d007dad50a508bd
keithyin <yinpenghhz@hotmail.com>
2025-07-08 01:39:47 +0800

    io uring 3

--- recfile-examples/src/block-device-io-benchmarking.rs
+++ recfile-examples/src/block-device-io-benchmarking.rs
@@ -7,7 +7,7 @@ use std::{
     time::Instant,
 };
 
-use io_uring::{IoUring, opcode, types};
+use io_uring::{cqueue, opcode, types, IoUring};
 use recfile::util::{
     aligned_alloc,
     buffer::{AlignedVecU8, Buffer},
@@ -211,6 +211,94 @@ fn io_uring2(fname: &str, data: &[u8], chunk_size: usize) {
     drop(file);
 }
 
+fn io_uring3(fname: &str, data: &[u8], chunk_size: usize) {
+    let file = fs::OpenOptions::new()
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .custom_flags(libc::O_DIRECT)
+        .open(fname)
+        .expect("Unable to create file");
+
+    let mut valid_idx_queue = FixedSizeStack::new(8);
+    valid_idx_queue = valid_idx_queue.fill_stack();
+
+    let io_depth = 8;
+    let real_buffers = (0..io_depth)
+        .into_iter()
+        .map(|_| RefCell::new(Buffer::new(chunk_size, get_page_size())))
+        .collect::<Vec<_>>();
+
+    let rio_buffers = real_buffers
+        .iter()
+        .map(|buf| libc::iovec {
+            iov_base: buf.borrow_mut().as_mut_ptr() as *mut _,
+            iov_len: chunk_size,
+        })
+        .collect::<Vec<_>>();
+    // let mut ring = IoUring::new(io_depth as u32).expect("Failed to create IoUring");
+    let mut ring = IoUring::builder()
+        .setup_sqpoll(1000)
+        .build(io_depth as u32)
+        .expect("Failed to create IoUring");
+
+    // init completions
+    let mut remaining_io = 0;
+
+    unsafe {
+        ring.submitter()
+            .register_buffers(rio_buffers.as_slice())
+            .unwrap();
+        ring.submitter()
+            .register_files(&[file.as_raw_fd()])
+            .unwrap();
+    }
+
+    let mut start = 0;
+
+    while start < data.len() {
+        // Write data to the file
+        let end = std::cmp::min(start + chunk_size, data.len());
+        if let Some(valid_idx) = valid_idx_queue.pop() {
+            let mut buffer = real_buffers[valid_idx].borrow_mut();
+            buffer.clear().push(&data[start..end]);
+            let write_event = opcode::WriteFixed::new(
+                types::Fixed(0),
+                buffer.as_mut_ptr(),
+                chunk_size as u32,
+                valid_idx as u16,
+            )
+            .offset(start as u64)
+            .build()
+            .user_data(valid_idx as u64);
+
+            unsafe {
+                ring.submission()
+                    .push(&write_event)
+                    .expect("Failed to push write event");
+            }
+            start = end;
+            remaining_io += 1;
+        } else {
+            ring.submit_and_wait(1).unwrap();
+            let cqe: cqueue::Entry = ring.completion().next().expect("No completion event");
+            if cqe.result() < 0 {
+                panic!("cqe result : {}", cqe.result());
+            }
+            remaining_io -= 1;
+            valid_idx_queue.push(cqe.user_data() as usize);
+        }
+    }
+    ring.submit_and_wait(remaining_io).unwrap();
+    while let Some(cqe) = ring.completion().next() {
+        if cqe.result() < 0 {
+            panic!("cqe error: {}", cqe.result());
+        }
+    }
+    file.sync_all().expect("Failed to sync file");
+    drop(file);
+}
+
 fn prepare_data() -> Vec<u8> {
     let mut file = fs::File::open("/dev/random").expect("open /dev/random failed");
     let data_size = 1024 * 1024 * 1024 * 10; // 10GB
@@ -263,10 +351,11 @@ fn benckmark(fname: &str) {
     let data = prepare_data();
     let chunk_size = 1024 * 1024; // 1MB
 
-    benchmarking_core("buffered_io", fname, &data, chunk_size, buffered_io);
+    // benchmarking_core("buffered_io", fname, &data, chunk_size, buffered_io);
     benchmarking_core("direct_io", fname, &data, chunk_size, direct_io);
     benchmarking_core("io_uring", fname, &data, chunk_size, io_uring);
     benchmarking_core("io_uring2", fname, &data, chunk_size, io_uring2);
+    benchmarking_core("io_uring3", fname, &data, chunk_size, io_uring3);
 }
 
 fn main() {
